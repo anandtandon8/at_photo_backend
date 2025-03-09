845e4c707f4b062f2213bbc9df9035e822f3b2dd
m3di0cre3 <anandtandon8@gmail.com>
2025-03-08 22:02:57 -0500

    updated scripts

--- scripts/stop-application.sh
+++ scripts/stop-application.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-if [ "$(docker ps -q --filter "name=atphotobackend")" ]; then
+if [ "$(docker ps -q -f name=^/atphotobackend$)" ]; then
+    echo "Stopping atphotobackend container..."
     sudo docker stop atphotobackend
+    sudo docker rm atphotobackend
+elif [ "$(docker ps -aq -f status=exited -f name=^/atphotobackend$)" ]; then
+    echo "Removing stopped atphotobackend container..."
     sudo docker rm atphotobackend
 fi 
