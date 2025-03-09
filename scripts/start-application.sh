0c3d52359513717f99dcef54484f8a74e0d266e4
m3di0cre3 <anandtandon8@gmail.com>
2025-03-08 22:27:37 -0500

    passed environment vars on docker run

--- scripts/start-application.sh
+++ scripts/start-application.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+echo "Starting atphotobackend container..."
 sudo docker run -d \
     --name atphotobackend \
     -p 3000:3000 \
     --restart unless-stopped \
+    -e NODE_ENV=production \
+    -e PORT=3000 \
     atphotobackend:Latest 
     
