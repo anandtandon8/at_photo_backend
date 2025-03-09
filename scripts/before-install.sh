f427f44b740efe6b4dd87a5d43e7af7ab9243b93
m3di0cre3 <anandtandon8@gmail.com>
2025-03-09 19:50:50 -0400

    anotha try:

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-rm -rf /var/www/html/atphotobackend/appspec.yml || true
-rm -rf /var/www/html/atphotobackend/scripts/* || true
+rm -rf /var/www/html/atphotobackend/*
+
+aws s3 cp s3://privatebucketforatphoto/postgres_pass.txt /var/www/html/atphotobackend/postgres_pass.txt
+aws s3 cp s3://privatebucketforatphoto/add_imgs_api_key.txt /var/www/html/atphotobackend/add_imgs_api_key.txt
+aws s3 cp s3://privatebucketforatphoto/us-east-2-bundle.pem /var/www/html/atphotobackend/us-east-2-bundle.pem
