597b7716354a14e0d8720915035053c3f31cdfab
m3di0cre3 <anandtandon8@gmail.com>
2025-03-09 20:11:14 -0400

    might work

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 rm -rf /var/www/html/atphotobackend/*
 
-aws s3 cp s3://privatebucketforatphoto/postgres_pass.txt /var/www/html/atphotobackend/postgres_pass.txt
-aws s3 cp s3://privatebucketforatphoto/add_imgs_api_key.txt /var/www/html/atphotobackend/add_imgs_api_key.txt
-aws s3 cp s3://privatebucketforatphoto/us-east-2-bundle.pem /var/www/html/atphotobackend/us-east-2-bundle.pem
