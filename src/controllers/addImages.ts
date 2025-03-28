d7880dca6c1e643fe5d5bb9bbf768116c7c5c0ae
m3di0cre3 <anandtandon8@gmail.com>
2025-03-27 23:24:02 -0400

    changed multer tmp location

--- src/controllers/addImages.ts
+++ src/controllers/addImages.ts
@@ -13,7 +13,7 @@ interface MulterRequest extends Request {
   file?: Express.Multer.File;
 }
 
-const upload = multer({ dest: '/tmp/' })
+const upload = multer({ dest: path.join(__dirname, '..', 'uploads') })
 
 async function getClassification(imageBuffer: Buffer): Promise<string> {
     const endpoint = 'https://s0ozky42g1.execute-api.us-east-2.amazonaws.com/default';
